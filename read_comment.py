59a3b019f396b5b82de83eed476007a5c08189e4
Toffrox <jeffrey.yflu@gmail.com>
2021-10-28 18:28:30 -0400

    exception handling

--- read_comment.py
+++ read_comment.py
@@ -8,16 +8,20 @@ rickrolls = 0
 r = praw.Reddit('RickRollRadar by /r/Toffrox')
 
 subreddit = r.subreddit("toffrox")
-# TODO: Add subreddit blacklist, exception handling when crashing
-for comment in subreddit.stream.comments(skip_existing=True):
-    comments_read += 1
-    print(r.auth.limits)
-    for l in links:
-        if l in comment.body:
-            if comment.author == "RickRollRadar":
-                break
-            print("Detected Rickroll: ")
-            print(comment.body)
-            rickrolls += 1
-            comment.reply("Warning! The comment above is a **rickroll**! Click at your own risk! \n \n ^(I am a bot, warning the world of all rickrolls. Out of " + str(comments_read) + " comments, I have detected " + str(rickrolls) + " rickrolls. You can find more information) [^(here)](https://www.youtube.com/watch?v=dQw4w9WgXcQ)^(.)")
-            print("Replied")
+# TODO: Add subreddit blacklist
+while True:
+    try:
+        for comment in subreddit.stream.comments(skip_existing=True):
+            comments_read += 1
+            print(r.auth.limits)
+            for l in links:
+                if l in comment.body:
+                    if comment.author == "RickRollRadar":
+                        break
+                    print("Detected Rickroll: ")
+                    print(comment.body)
+                    rickrolls += 1
+                    comment.reply("Warning! The comment above is a **rickroll**! Click at your own risk! \n \n ^(I am a bot, warning the world of all rickrolls. Out of " + str(comments_read) + " comments, I have detected " + str(rickrolls) + " rickrolls. You can find more information) [^(here)](https://www.youtube.com/watch?v=dQw4w9WgXcQ)^(.)")
+                    print("Replied")
+    except Exception as e:
+        print(str(e))
